4dc0a1b21cf8c2a0f823c1d0c261f8651d4861ea
Nicholas Ferreira <nicholasjferreira98@gmail.com>
2026-02-17 21:08:06 -0800

    Finished Hero Page

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,46 @@
+import { Rocket, Sparkles, Users, Wrench } from "lucide-react";
+
+const highlights = [
+    {
+    icon: Users,
+    title: "Collaboration",
+    description: "I communicate and collaborate across academics, research, work experience, and hackathons.",
+    },
+
+    {
+        icon: Rocket,
+        title: "Purpose-Driven",
+        description: "Every product I build is designed to solve a problem and make a positive impact on others' lives.",
+        },
+    
+    {
+        icon: Sparkles,
+        title: "Continuous Learner",
+        description: "I'm committed to growing my skills and knowledge in unfamiliar technologies, and I thrive in new environments.",
+        },
+
+    {
+        icon: Wrench,
+        title: "Engineering Quality",
+        description: "I write reliable code with testing (PyTest), edge-case handling, and clear structure to ease the debugging process.",
+        },
+];
+
 
 export const About = () => {
 
-    return <section></section>
+    return (
+        <section id="about" className="py-32 relative overflow-hidden">
+            <div>
+                <div>
+                {/* Left Column */}
+                    <div>
+                        <span>About Me</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+        )
 
 
 }
